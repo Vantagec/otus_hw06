3ccd6ccd4cb91da8c571570ec0ec54d79ba17692
Vantagec <163841699+Vantagec@users.noreply.github.com>
2024-07-30 20:24:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,29 +3,33 @@
 #include <matrix.hpp>
 #include "version.h"
 
-using namespace std;
-
 int main()
 {
-    using namespace std;
+    using std::cout;
+    using std::endl;
+    using std::size_t;
 
     cout << "Application version: " << PROJECT_VERSION << endl;
 
+    // Создание матрицы размера 10x10 с элементами по умолчанию равными 0
     Matrix<int, 0> matrix;
-    for (std::size_t i = 0; i < 10; ++i) {
-        matrix[i][i] = i;
-        matrix[i][9 - i] = 9 - i;
+    for (size_t i = 0; i < 10; ++i) {
+        matrix[i][i] = i;           // Заполняем диагональ элементами от 0 до 9
+        matrix[i][9 - i] = 9 - i;   // Заполняем обратную диагональ
     }
 
-    for (std::size_t r = 1; r < 9; ++r) {
-        std::cout << matrix[r][1];
-        for (std::size_t c = 2; c < 9; ++c) {
-            std::cout << " " << matrix[r][c];
+    // Печатаем элементы матрицы в диапазоне строк от 1 до 8 и столбцов от 1 до 8
+    for (size_t row = 1; row < 9; ++row) {
+        for (size_t col = 1; col < 9; ++col) {
+            cout << matrix[row][col];
+            if (col < 8) cout << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 
-    std::cout << matrix.size() << std::endl;
+    // Выводим текущий размер матрицы
+    cout << "Matrix size: " << matrix.size() << endl;
 
     return 0;
 }
+
